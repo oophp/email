08f79b6a9c298ea62002787ec0a550a83805d683
Yehuda Deutsch <yeh@uda.co.il>
2016-06-26 11:55:38 +0300

    Add defaults to None header type

--- src/Message/Header/None.php
+++ src/Message/Header/None.php
@@ -6,6 +6,11 @@ use OOPHP\Email\HeaderField;
 
 class None extends HeaderField
 {
+    public function __construct($name = '', $value = '')
+    {
+        parent::__construct($name, $value);
+    }
+
     public function addValue($value)
     {
         return $this;
